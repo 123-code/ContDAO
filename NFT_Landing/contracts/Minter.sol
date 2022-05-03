e1c797db15afb3de46b9199e7622c490185117fc
123-code <naranjojose256@gmail.com>
2022-05-03 17:49:20 -0500

    First Commit

--- NFT_Landing/contracts/Minter.sol
+++ NFT_Landing/contracts/Minter.sol
@@ -1,8 +1,23 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 contract Minter is ERC721 {
+using SafeMath for uint256;
+ address public Owner;
+ string public image;
+ string public Name;
+string public Symbol;
+
+constructor() ERC721(Name,Symbol){
+
+}
+
+function mint(string memory uri)public payable {
+
+}
+ mapping(address=>string)public owners;
 
 }
