53ea99eb7ed0bfb7dfcc5a8411c65ba7b891f610
123-code <naranjojose256@gmail.com>
2022-10-12 17:15:46 -0500

    Added basic functionality

--- NFT_Landing/landing/src/Pages/Main.js
+++ NFT_Landing/landing/src/Pages/Main.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import Connectbutton from '../Components/Connectbutton';
 import {FaDiscord} from 'react-icons/fa';
 import{AiFillFacebook} from 'react-icons/ai';
@@ -7,12 +7,80 @@ import {AiFillGithub} from 'react-icons/ai';
 import {BsTwitter} from 'react-icons/bs';
 import Description from '../Components/Description';
 import Footer from '../Components/Footer';
+import Web3Modal from 'web3modal';
 
 // ">
+
 const Main = ()=>{
+
+const Web3ModalRef = useRef();
+const [walletConnected,setwalletConnected] = useState(false);
+
+const connectwallet = async () => {
+    try {
+
+      await getProviderOrSigner();
+      setwalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    
+    const provider = await Web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to goerli");
+      throw new Error("Change network to goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  useEffect(()=>{
+    if(!walletConnected){
+      Web3ModalRef.current = new Web3Modal({
+        network:"rinkeby",
+        providerOptions:{},
+        disableInjectedProvider:false,
+      });
+      connectwallet();
+    }
+
+
+  },[])
+
+
+  const showbuttons = async()=>{
+    if(!walletConnected){
+        return(
+            <button onClick={connectwallet}> Conectar Billetera </button>
+        )
+    }
+
+    else{
+        return(
+            <buttton>conectada</buttton>
+        )
+    }
+  }
+
+
+
  let[icono,seticono] = useState(<AiFillFacebook/>);
  
 return(
+    <>
+    {showbuttons()}
 <div className="bg-gradient-to-t from-blue-400 to-emerald-400">
 <div classname = "font-mono">
 <Description/>
@@ -20,7 +88,7 @@ return(
 
 <div >
 
-<Connectbutton/>
+
 
 <div>
     <h1 className="font-mono"> Reach us At: </h1> 
@@ -30,6 +98,7 @@ return(
 </div>
 
 </div>
+</>
 )
 } 
 export default Main;
@@ -84,3 +153,4 @@ const iconos = ()=>{
       
 
     
+// macro 
