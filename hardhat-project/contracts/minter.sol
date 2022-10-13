f65d3188d0a8e603afa4843381c2b962d584048d
123-code <naranjojose256@gmail.com>
2022-10-13 12:25:47 -0500

    Hardhat project added

---
+++ hardhat-project/contracts/minter.sol
@@ -0,0 +1,32 @@
+pragma solidity ^ 0.8.9;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract minter is ERC721,Ownable{
+    string public baseuri;
+    bool public presalestarted = false;
+    uint256 public maxpresalecap = 0;
+
+
+    constructor(string memory _baseuri)ERC721("ECTOKEN","EC"){
+        baseuri = _baseuri;
+
+    }
+
+function setmaxpresalecap(uint2566 _maxpresalecap)public onlyOwner{
+    maxpresalecap = _maxpresalecap;
+}
+
+function startpresale()public onlyOwner{
+    presalestarted = true;
+}
+
+
+function presalemint() public payable{
+
+}
+
+
+
+
+    }
+
