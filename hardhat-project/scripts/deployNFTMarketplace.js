51f579cb3b5d35bb472a17e15ab730cb698dff36
123-code <naranjojose256@gmail.com>
2023-01-08 13:28:12 -0500

    repo updates

---
+++ hardhat-project/scripts/deployNFTMarketplace.js
@@ -0,0 +1,21 @@
+const hre = require('hardhat');
+
+async function main() {
+
+
+  
+    const NFTContfract = await hre.ethers.getContractFactory("NFTMarketplace");
+    const nftcontract = await NFTContfract.deploy();
+  
+    await nftcontract.deployed();
+  
+    console.log(
+      `contract deployed to ${nftcontract.address}`
+    );
+  }
+  
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+  
