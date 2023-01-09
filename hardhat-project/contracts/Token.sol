99e8d27cae8c6b4aba6765fa0649b11d85a084b3
123-code <naranjojose256@gmail.com>
2023-01-09 11:34:33 -0500

    token Burn Function

--- hardhat-project/contracts/Token.sol
+++ hardhat-project/contracts/Token.sol
@@ -8,19 +8,22 @@ contract MyToken is ERC721URIStorage {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    constructor() ERC721("ContributeToken", "CTK") {}
+    mapping(address=>uint) public tokencount;
+    uint immutable price = 0.001 ether;
 
-    constructor() ERC721("ContributeToken", "CTK") {
-
-    }
+event Burned(address indexed owner, uint256 indexed tokenId);
 
 function mint(address _to,string memory TokenURI) external returns(uint){
 _tokenIds.increment();
 uint newItemID = _tokenIds.current();
 _mint(_to,newItemID);
 _setTokenURI(newItemID,TokenURI);
+tokencount[_to] += 1;
 return newItemID;
 }
-
-
-
+function burn(uint _tokenID)external{
+_burn(_tokenID);
+emit Burned(msg.sender,_tokenID);
+}
 }
