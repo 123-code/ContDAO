f3661b938c4e3d66c3068d9072f9a14ceacaf19d
123-code <naranjojose256@gmail.com>
2023-03-05 12:16:31 -0500

    _-_

--- landing/pages/AddProposal.js
+++ landing/pages/AddProposal.js
@@ -1,34 +1,33 @@
-
 import ConnectButton from './Components/ConnectWalllet'
 import React,{useState} from 'react';
 
 
 export default function Home() {
-  //const [isConnected, setIsConnected] = useState(false);
   const [ProjectName, setProjectName] = useState('');
   const [ProjectDescription, setProjectDescription] = useState('');
   const [Location,setLocation] = useState('');
   const [fundingGoal,setFundingGoal] = useState(0);
+
   return (
-   <>
-<div>
-  <h1> Add Proposal</h1>
-
-  <label>Project Name</label>
-  <input type="text" value={ProjectName} onChange={(e) => setProjectName(e.target.value)} />
-
-  <label>Project Description</label>
-  <input type="text" value={ProjectDescription} onChange={(e) => setProjectDescription(e.target.value)} />
-
-  <label>Location</label>
-  <input type="text" value={Location} onChange={(e) => setLocation(e.target.value)} />
-
-  <label>Funding Goal</label>
-  <input type="number" value={fundingGoal} onChange={(e) => setFundingGoal(e.target.value)} />
-
-  <button onClick={() => console.log(ProjectName,ProjectDescription,Location,fundingGoal)}>Add Proposal</button>
-</div>
-
-   </>
+    <div style={{ fontFamily: 'Futura, sans-serif' }}>
+      <h1>Add Proposal</h1>
+      <div className="form-item">
+        <label className="Font">Project Name</label>
+        <input type="text" value={ProjectName} onChange={(e) => setProjectName(e.target.value)} />
+      </div>
+      <div className="form-item">
+        <label className="Font">Project Description</label>
+        <input type="text" value={ProjectDescription} onChange={(e) => setProjectDescription(e.target.value)} />
+      </div>
+      <div className="form-item">
+        <label>Location</label>
+        <input type="text" value={Location} onChange={(e) => setLocation(e.target.value)} />
+      </div>
+      <div className="form-item">
+        <label>Funding Goal</label>
+        <input type="number" value={fundingGoal} onChange={(e) => setFundingGoal(e.target.value)} />
+      </div>
+      <button onClick={() => console.log(ProjectName,ProjectDescription,Location,fundingGoal)}>Post</button>
+    </div>
   )
 }
